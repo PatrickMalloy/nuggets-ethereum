92a47528fee8d6bec496ee0110c2834ed92f07be
Patrick Malloy <6231534+PatrickMalloy@users.noreply.github.com>
2023-12-08 10:29:45 -0500

    added ability to create multiple wallets and to save to .env file

--- create-wallet/createWallet.js
+++ create-wallet/createWallet.js
@@ -1,23 +1,44 @@
 import { Wallet, getDefaultProvider } from "ethers";
+import fs from "fs";
 
 // Use Ethers DefaultProvider for the Ethereum Sepolia Testnet
 const provider = getDefaultProvider("sepolia");
 
-function createWallet() {
-  // Create a new random wallet.
-  const walletRandom = Wallet.createRandom();
-  logWallet(walletRandom, "From Random");
+//let accountNum = 0;
 
-  // Create a new wallet using the Mnemonic from the first wallet.
-  // This simulates doing a wallet restore function
-  const walletPhrase = Wallet.fromPhrase(walletRandom.mnemonic.phrase);
-  logWallet(walletPhrase, "From Mnemonic");
+function createWallet(_numAccounts) {
+  for (let count=1; count <= _numAccounts; count++ ) {
+    // Create a new random Ethereum account.
+    const walletRandom = Wallet.createRandom();
+    //accountNum++;
+    console.log(`[ACCOUNT_${count}]`);
+    saveAccount(walletRandom, `[ACCOUNT_${count}]`);
+    logWallet(walletRandom, "From Random");
 
-  // Create a new wallet using the Private Key from the first wallet.
-  // This can also be used to restore a wallet. Notice though you don't
-  // get a Mnemonic Phrase when creating from a Private Key
-  const walletPrivate = new Wallet(walletRandom.privateKey);
-  logWallet(walletPrivate, "From Private");
+    // Create a new wallet using the Mnemonic from the first wallet.
+    // This simulates doing a wallet restore function
+    const walletPhrase = Wallet.fromPhrase(walletRandom.mnemonic.phrase);
+    logWallet(walletPhrase, "From Mnemonic");
+
+    // Create a new wallet using the Private Key from the first wallet.
+    // This can also be used to restore a wallet. Notice though you don't
+    // get a Mnemonic Phrase when creating from a Private Key
+    const walletPrivate = new Wallet(walletRandom.privateKey);
+    logWallet(walletPrivate, "From Private");
+  }
+}
+
+function saveAccount(_wallet, _header) {
+  const content = _header + "\n" +
+  "ADDRESS='" + _wallet.address + "'\n" +
+  "PRIVATE_KEY='" + _wallet.privateKey + "'\n" +
+  "MNEMONIC='" + _wallet.mnemonic.phrase + "'\n\n";
+
+  fs.writeFile('./.env', content, { flag: 'a'}, err => {
+    if(err) {
+      console.log(err);
+    }
+  });
 }
 
 function logWallet(wallet, msg) {
@@ -30,4 +51,4 @@ if( wallet.mnemonic ) {
 console.log();
 }
 
-createWallet();
+createWallet(3);
